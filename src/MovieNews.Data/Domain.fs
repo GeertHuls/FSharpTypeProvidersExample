e6f19671f04317895fc84d2b129149754400e58c
Geert Huls <geert.huls82@gmail.com>
2017-07-09 22:22:50 +0200

    Created MoviesData class lib
    - domain types
    - sample json data files

---
+++ src/MovieNews.Data/Domain.fs
@@ -0,0 +1,34 @@
+﻿namespace MovieNews.Data
+
+// Namespace can only contain types but not functions.
+type MovieBasics =
+    {   Title : string
+        Summary : string
+        Thumbnail : option<string> }
+
+type Review =
+  { Published : System.DateTime
+    Summary : string
+    Link : string
+    LinkText : string }
+
+type Cast =
+  { Actor : string
+    Character : string }
+
+type Details = 
+  { Homepage : string
+    Genres : seq<string>
+    Overview : string
+    Companies : seq<string>
+    Poster : string
+    Countries : seq<string>
+    Released : System.DateTime
+    AverageVote : decimal }
+
+/// Combines all available information about a movie
+type Movie =
+  { Movie : MovieBasics
+    Details : Details
+    Cast : seq<Cast>
+    Review : option<Review> }
