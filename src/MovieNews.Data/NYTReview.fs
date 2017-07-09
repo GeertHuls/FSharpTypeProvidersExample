7c2389280f80894d08b7bb804e385fcf54d1daa8
Geert Huls <geert.huls82@gmail.com>
2017-07-09 22:29:58 +0200

    Added the netflix and NYT review functions

---
+++ src/MovieNews.Data/NYTReview.fs
@@ -0,0 +1,28 @@
+﻿module MovieNews.Data.NYTReview
+open FSharp.Data
+
+type NYT = JsonProvider<"http://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=2ef8ce1a9c93a64599d9d00f80555ff3:8:72646066&query=paddington">
+
+let baseUrl = "http://api.nytimes.com/svc/movies/v2/reviews/search.json"
+let apiKey = "2ef8ce1a9c93a64599d9d00f80555ff3:8:72646066"
+
+/// As in 'Netflix.fs', the following function takes the
+/// response as input and so it can be easily tested.
+
+let tryPickReviewByName name response =
+  let nyt = NYT.Parse(response)
+  let reviewOpt = nyt.Results |> Seq.tryFind (fun r ->
+    System.String.Equals
+      ( r.DisplayTitle, name, 
+        System.StringComparison.InvariantCultureIgnoreCase) )
+
+  reviewOpt |> Option.map (fun r ->
+    { Published = r.PublicationDate
+      Summary = r.SummaryShort
+      Link = r.Link.Url
+      LinkText = r.Link.SuggestedLinkText } )
+
+let tryDownloadReviewByNme name = 
+  let q = ["api-key", apiKey; "query", name]
+  let response = Http.RequestString(baseUrl, q)
+  tryPickReviewByName name response
---
+++ src/MovieNews.Data/Netflix.fs
@@ -0,0 +1,31 @@
+﻿module MovieNews.Data.Netflix
+open FSharp.Data
+open System.Text.RegularExpressions
+
+let regexThumb = 
+  Regex("<a[^>]*><img src=\"([^\"]*)\".*>(.*)")
+
+type Netflix = 
+  XmlProvider<"http://dvd.netflix.com/Top100RSS">
+
+/// The function takes a response from Netflix and returns the 
+/// parsed data as a sequence (IEnuemerable). This makes it
+/// easy to test from C# unit test project.
+let parseTop100 response =
+  let top = Netflix.Parse(response)
+  seq {
+    for it in top.Channel.Items ->
+      let m = regexThumb.Match(it.Description)
+      let descr, thumb = 
+        if m.Success then
+          m.Groups.[2].Value,
+          Some(m.Groups.[1].Value)
+        else it.Description, None
+      { Title = it.Title 
+        Summary = descr
+        Thumbnail = thumb } }
+
+let getTop100 () = 
+  parseTop100 (Http.RequestString("http://dvd.netflix.com/Top100RSS"))
+
+
